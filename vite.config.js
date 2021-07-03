a2e6e0d2aa019710c9e57b306f18ea21f2b0a917
rieg-ec <rieg@uc.cl>
2021-07-03 15:35:07 -0400

    refactor: remove windicss

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import WindiCSS from 'vite-plugin-windicss';
 
 export default () =>
   defineConfig({
-    plugins: [vue(), WindiCSS()],
+    plugins: [vue()],
     server: {
       watch: {
         usePolling: true,
