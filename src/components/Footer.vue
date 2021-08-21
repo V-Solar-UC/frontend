b62ed4ac89b483f5cb09b64f3c7e65824d02ce7e
rieg-ec <rieg@uc.cl>
2021-08-21 16:03:41 -0400

    feat(footer): add rrss icons and year function

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,14 +1,23 @@
 <template>
-  <div class="flex flex-col bg-red-200 w-full h-48 text-center">
-    <div class="text-center w-full flex-grow flex flex-col items-center justify-center">
-      <div class="btn-red-round">conocenos</div>
-      <div class="flex flex-row justify-between w-48 mt-6">
-        <div>gh</div>
-        <div>twitter</div>
-        <div>linkedin</div>
+  <div class="flex flex-col bg-gray-50 w-full h-48 text-center">
+    <div class="text-center w-full h-full flex flex-col items-center justify-center space-y-5 pt-6">
+      <button class="btn-red-round">conocenos</button>
+      <div class="flex flex-row justify-between space-x-6 mt-6">
+        <a href="https://github.com/V-Solar-UC">
+          <font-awesome-icon :icon="['fab', 'github']" class="icon" />
+        </a>
+        <a href="https://instagram.com/vsolar_uc">
+          <font-awesome-icon :icon="['fab', 'instagram']" class="icon" />
+        </a>
+        <a href="https://www.linkedin.com/company/v-solar-uc">
+          <font-awesome-icon :icon="['fab', 'linkedin']" class="icon" />
+        </a>
+        <a href="mailto:v-solar@ing.puc.cl">
+          <font-awesome-icon :icon="['far', 'envelope']" class="icon" />
+        </a>
       </div>
     </div>
-    <div class="text-center w-full bg-red-500">
+    <div class="text-center w-full bg-red-500 text-base py-2 text-white">
       V-Solar {{ year }} @ Todos los derechos reservados
     </div>
   </div>
@@ -19,8 +28,15 @@ export default {
   name: 'Footer',
   components: {},
   setup() {
-    const year = 2021; // TODO: get current year
+    const year = new Date().getFullYear();
     return { year };
   },
 };
 </script>
+
+<style>
+.icon {
+  @apply text-3xl;
+}
+</style>
+
